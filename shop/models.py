28a79f01e5276f9e1ce9e61c38fda3ab9a93b265
gautamamber <ambergautam1@gmail.com>
2018-09-04 01:11:39 +0530

    Listing and details of products

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -14,6 +14,9 @@ class Category(models.Model):
 
 	def __str__(self):
 		return self.name
+
+	def get_absolute_url(self):
+		return reverse('shop:product_list_by_category' , args = [self.slug])
 
 class Products(models.Model):
 	category = models.ForeignKey(Category, related_name = 'products' , on_delete = models.CASCADE)
@@ -33,3 +36,6 @@ class Products(models.Model):
 
 	def __str__(self):
 		return self.name
+
+	def get_absolute_url(self):
+		return reverse('shop:product_detail' , args = [self.id, self.slug])
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_onject_or_404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -12,7 +12,7 @@ def product_list(request, category_slug = None):
 	categories = Category.objects.all()
 	products = Products.objects.filter(available = True)
 	if category_slug:
-		category = get_onject_or_404(Category, slug = category_slug)
+		category = get_object_or_404(Category, slug = category_slug)
 		products = Products.objects.filter(category = category)
 
 	context = {
@@ -20,13 +20,13 @@ def product_list(request, category_slug = None):
 	'categories' : categories,
 	'products' : products
 	}
-	return render(request, 'shop/products/list.html', context)
+	return render(request, 'shop/product/list.html', context)
 
 def product_detail(request, id, slug):
-	product = get_onject_or_404(Products, id = id, slug = slug, available= True)
+	product = get_object_or_404(Products, id = id, slug = slug, available= True)
 	context = {
 	'product' : product
 	}
-	return render(request, 'shop/product/detail.html', context)
+	return render(request, 'shop/product/details.html', context)
 
 
