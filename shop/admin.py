71fd4de899b8a9f0d5dc396578aa879b174fbc96
gautamamber <ambergautam1@gmail.com>
2018-09-03 23:44:45 +0530

    Register category and products model in the admin site, created view to display list of products and a single product

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Products
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name' , 'slug']
+	prepopulated_fields = {
+	'slug' : ('name',)
+	}
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug' , 'price' , 'stock', 'available', 'created_at', 'updated_at']
+	list_filter = ['available', 'created_at', 'updated_at']
+	list_editable  = ['price', 'stock', 'available']
+	prepopulated_fields = {'slug' : ('name',)}
+
+admin.site.register(Products, ProductAdmin)
+
+
+#prepopulated_fields attribute to specify fields where the value is automatically set using the value of another field
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_onject_or_404
 
 # Create your views here.
+
+from .models import Category, Products
+
+#product_list use to display a list of products
+# category_slug=None use if products are filtered using a given category by our users.
+
+def product_list(request, category_slug = None):
+	category = None
+	categories = Category.objects.all()
+	products = Products.objects.filter(available = True)
+	if category_slug:
+		category = get_onject_or_404(Category, slug = category_slug)
+		products = Products.objects.filter(category = category)
+
+	context = {
+	'category' : category,
+	'categories' : categories,
+	'products' : products
+	}
+	return render(request, 'shop/products/list.html', context)
+
+def product_detail(request, id, slug):
+	product = get_onject_or_404(Products, id = id, slug = slug, available= True)
+	context = {
+	'product' : product
+	}
+	return render(request, 'shop/product/detail.html', context)
+
+
